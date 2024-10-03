b9b3c57e2cc1a324c5d4a401edc4198d90870e58
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-03 12:41:13 +0200

    create environment app

--- create_environment.sh
+++ create_environment.sh
@@ -30,25 +30,24 @@ touch "$script_dir/submission_reminder_app/assets/submissions.txt"
 
 touch "$script_dir/submission_reminder_app/config/config.env"
 
-cp "$script_dir/submissions.txt" "$script_dir/submission_reminder_app/assets/submissions.txt"
-
-echo "Jacky, Shell Navigation,not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
-
-echo "Batoni, Shell Navigation,  submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
-
-echo "lisa, Shell Navigation, not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
-
-echo "Hero, Shell Navigation, submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
-
-echo "carla, Shell Navigation, not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
-
 cat "$script_dir/reminder.sh" >> "$script_dir/submission_reminder_app/app/reminder.sh"
 
 cat "$script_dir/functions.sh" >> "$script_dir/submission_reminder_app/modules/functions.sh"
 
 cat "$script_dir/config.env" >> "$script_dir/submission_reminder_app/config/config.env"
 
-cat "$script_dir/startup.sh" >> "$script_dir/submission_reminder_app/startup.sh"
+cp "$script_dir/submissions.txt" "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Jacky,Shell Navigation,not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Batoni,Shell Navigation,submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "lisa,Shell Navigation,not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Hero,Shell Navigation,submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "carla,Shell Navigation,not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
 chmod u+x "$script_dir/submission_reminder_app/app/reminder.sh"
 
 chmod u+x "$script_dir/submission_reminder_app/modules/functions.sh"
--- submission_reminder_app/startup.sh
+++ submission_reminder_app/startup.sh
@@ -2,6 +2,8 @@
 
 script_dir="$(dirname "$0")"
 
+"$script_dir/../create_environment.sh"
+
 echo "Be ready for the reminder startup application"
 
 "$script_dir/app/reminder.sh"
