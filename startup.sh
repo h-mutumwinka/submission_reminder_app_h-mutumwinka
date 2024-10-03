b778a5512ae7b8d79e65e0ea61bdef7799bf26d4
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-03 16:00:44 +0200

    refactored the entire implementation

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_dir="$(dirname "$0")"
-
-echo "Be ready for the reminder startup application"
-
-"$script_dir/app/reminder.sh"
--- submission_reminder_app/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir="$(dirname "$0")"
-
-"$script_dir/../create_environment.sh"
-
-echo "Be ready for the reminder startup application"
-
-"$script_dir/app/reminder.sh"
